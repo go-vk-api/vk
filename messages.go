5fb764417dcf53bd0151ac14e2fd09d2995bf2a3
urShadow <videnex@gmail.com>
2016-05-29 20:02:18 +0300

    added files

---
+++ messages.go
@@ -0,0 +1,30 @@
+﻿package vk
+
+import (
+	"encoding/json"
+)
+
+type messages struct {
+	client *VK
+}
+
+// Send https://new.vk.com/dev/messages.send
+func (messages *messages) Send(params RequestParams) (int64, error) {
+	resp, err := messages.client.CallMethod("messages.send", params)
+
+	if err != nil {
+		return 0, err
+	}
+
+	type JSONBody struct {
+		MessageID int64 `json:"response"`
+	}
+
+	var body JSONBody
+
+	if err := json.Unmarshal(resp, &body); err != nil {
+		return 0, err
+	}
+
+	return body.MessageID, nil
+}
